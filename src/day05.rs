c8b86466080279e8cfb7411c01128186393dbec6
Marvin <marvk3@gmail.com>
2023-12-05 19:21:08 +0100

    5 refactor

--- src/day05.rs
+++ src/day05.rs
@@ -1,6 +1,3 @@
-#![allow(warnings, unused)]
-
-use std::cmp::min;
 use std::ops::Range;
 use std::str::FromStr;
 
@@ -18,13 +15,17 @@ impl Part<i64> for Part1 {
     }
 
     fn solve(&self, input: &[String]) -> i64 {
-        let (mut seeds, maps) = parse(input);
+        let (seeds, mappings) = parse(input);
 
-        for map in maps {
-            seeds = seeds.iter().map(|&num| map.convert(num)).collect::<Vec<_>>()
-        }
-
-        seeds.into_iter().min().unwrap()
+        mappings.iter()
+            .fold(seeds, |seeds, mapping| {
+                seeds.iter()
+                    .map(|&num| mapping.convert(num))
+                    .collect::<Vec<_>>()
+            })
+            .into_iter()
+            .min()
+            .unwrap()
     }
 }
 
@@ -37,23 +38,22 @@ impl Part<i64> for Part2 {
 
     fn solve(&self, input: &[String]) -> i64 {
         let (seeds, maps) = parse(input);
-        let converter = Converter::from(maps.as_slice());
+
+        let flattened_ranges = FlattenedRanges::from(maps.as_slice());
         let seed_ranges = build_seed_ranges(seeds.as_slice());
 
         seed_ranges
             .into_iter()
-            .map(|s| converter.min(s))
+            .map(|s| flattened_ranges.min(s))
             .min()
             .unwrap()
     }
 }
 
-fn parse(input: &[String]) -> (Vec<i64>, Vec<Map>) {
-    let mut split = input.split(|l| l.is_empty()).filter(|arr| !arr.is_empty());
+fn parse(input: &[String]) -> (Vec<i64>, Vec<Mapping>) {
+    let mut elements = input.split(|l| l.is_empty()).filter(|arr| !arr.is_empty());
 
-
-    let option = split.next();
-    let seeds = option
+    let seeds = elements.next()
         .unwrap()
         .first()
         .unwrap()
@@ -65,111 +65,100 @@ fn parse(input: &[String]) -> (Vec<i64>, Vec<Map>) {
         .map(|s| s.parse::<i64>().unwrap())
         .collect::<Vec<_>>();
 
-    let vec = split.map(Map::from).collect::<Vec<_>>();
+    let maps = elements.map(Mapping::from).collect::<Vec<_>>();
 
-    (seeds, vec)
+    (seeds, maps)
 }
 
-struct Converter {
+struct FlattenedRanges {
     ranges: Vec<(Range<i64>, i64)>,
 }
 
-impl Converter {
+impl FlattenedRanges {
     fn new(ranges: Vec<(Range<i64>, i64)>) -> Self {
         Self { ranges }
     }
 
     fn min(&self, seed_range: Range<i64>) -> i64 {
-        let mut result = i64::MAX;
-
-        for (range, offset) in &self.ranges {
-            let seed_range_start = seed_range.start;
-            let seed_range_end_exclusive = seed_range.end;
-            let seed_range_end_inclusive = seed_range.end - 1;
-
-            if seed_range.contains(&range.start) {
-                result = min(result, range.start + offset);
-            } else if seed_range.contains(&range.end) {
-                result = min(result, seed_range.start + offset)
-            }
-
-        }
-
-        result
+        self.ranges.iter()
+            .filter_map(|(range, offset)|
+                if seed_range.contains(&range.start) {
+                    Some(range.start + offset)
+                } else if seed_range.contains(&range.end) {
+                    Some(seed_range.start + offset)
+                } else {
+                    None
+                }
+            )
+            .min()
+            .unwrap()
     }
 }
 
 #[allow(clippy::never_loop)]
-impl From<&[Map]> for Converter {
-    fn from(value: &[Map]) -> Self {
-        let start = -(u32::MAX as i64) * 10;
-        let end = (u32::MAX as i64) * 10;
-        let mut ranges = vec![(start..end, 0_i64)];
-
-
-
-        for map in value {
-            ranges = ranges.into_iter().flat_map(|r| split(r, &map.references)).collect::<Vec<_>>();
-        }
+impl From<&[Mapping]> for FlattenedRanges {
+    fn from(value: &[Mapping]) -> Self {
+        let ranges =
+            value.iter()
+                .fold(vec![(i64::MIN..i64::MAX, 0_i64)], |ranges, map| {
+                    ranges.into_iter()
+                        .flat_map(|r| split(r, &map.references))
+                        .collect::<Vec<_>>()
+                });
 
         Self::new(ranges)
     }
 }
 
-fn split(range_and_offset: (Range<i64>, i64), map_ranges: &[MapRange]) -> Vec<(Range<i64>, i64)> {
-    let mut result = vec![range_and_offset];
-
+fn split(range_and_offset: (Range<i64>, i64), map_ranges: &[MappingRange]) -> Vec<(Range<i64>, i64)> {
     let mut result_from_ranges = vec![];
 
-    for map_range in map_ranges {
-        let map_range_start = map_range.source_range_start;
-        let map_range_end_exclusive = map_range.source_range_start + map_range.range_length;
-        let map_range_end_inclusive = map_range_end_exclusive - 1;
-        let map_range_offset = map_range.destination_range_start - map_range_start;
+    let mut result =
+        map_ranges.iter()
+            .fold(vec![range_and_offset], |result, map_range| {
+                result.iter()
+                    .flat_map(|(range, offset)| {
+                        let offset = *offset;
 
-        result =
-            result.iter()
-                .flat_map(|(range, offset)| {
-                    let test_range = (range.start + offset)..(range.end + offset);
+                        let mapping_start = map_range.source_range_start - offset;
+                        let mapping_end = map_range.source_range_start + map_range.range_length - offset;
+                        let mapping_offset = map_range.destination_range_start - map_range.source_range_start;
 
-                    let r = if test_range.contains(&map_range_start) && test_range.contains(&map_range_end_inclusive) {
-                        result_from_ranges.push((map_range_start - *offset..map_range_end_exclusive - *offset, *offset + map_range_offset));
+                        if range.contains(&mapping_start) && range.contains(&(mapping_end - 1)) {
+                            result_from_ranges.push((mapping_start..mapping_end, offset + mapping_offset));
 
-                        vec![
-                            (test_range.start..map_range_start, *offset),
-                            (map_range_end_exclusive..test_range.end, *offset),
-                        ]
-                    } else if test_range.contains(&map_range_start) {
-                        result_from_ranges.push((map_range_start - *offset..test_range.end - *offset, *offset + map_range_offset));
+                            vec![
+                                (range.start..mapping_start, offset),
+                                (mapping_end..range.end, offset),
+                            ]
+                        } else if range.contains(&mapping_start) {
+                            result_from_ranges.push((mapping_start..range.end, offset + mapping_offset));
 
-                        vec![
-                            (test_range.start..map_range_start, *offset),
-                        ]
-                    } else if test_range.contains(&map_range_end_inclusive) {
-                        result_from_ranges.push((test_range.start - *offset..map_range_end_exclusive - *offset, *offset + map_range_offset));
+                            vec![
+                                (range.start..mapping_start, offset),
+                            ]
+                        } else if range.contains(&(mapping_end - 1)) {
+                            result_from_ranges.push((range.start..mapping_end, offset + mapping_offset));
 
-                        vec![
-                            (map_range_end_exclusive..test_range.end, *offset),
-                        ]
-                    } else if test_range.start >= map_range_start && test_range.end <= map_range_end_exclusive {
-                        result_from_ranges.push((test_range.start - *offset..test_range.end - *offset, *offset + map_range_offset));
+                            vec![
+                                (mapping_end..range.end, offset),
+                            ]
+                        } else if range.start >= mapping_start && range.end <= mapping_end {
+                            result_from_ranges.push((range.clone(), offset + mapping_offset));
 
-                        vec![]
-                    } else {
-                        vec![
-                            (test_range.clone(), *offset),
-                        ]
-                    };
+                            vec![]
+                        } else {
+                            vec![
+                                (range.clone(), offset),
+                            ]
+                        }
+                    })
+                    .filter(|(r, _)| !r.is_empty())
+                    .collect::<Vec<_>>()
+            });
 
-                    r.into_iter().map(|(r, o)| (r.start - *offset..r.end - *offset, o))
-                })
-                .filter(|(r, _)| !r.is_empty())
-                .collect::<Vec<_>>();
-    }
 
     result.append(&mut result_from_ranges);
-
-    result.sort_by_key(|r| r.0.start);
 
     result
 }
@@ -181,15 +170,13 @@ fn build_seed_ranges(nums: &[i64]) -> Vec<Range<i64>> {
 }
 
 #[derive(Debug)]
-struct Map {
-    from: String,
-    to: String,
-    references: Vec<MapRange>,
+struct Mapping {
+    references: Vec<MappingRange>,
 }
 
-impl Map {
-    fn new(from: String, to: String, references: Vec<MapRange>) -> Self {
-        Self { from, to, references }
+impl Mapping {
+    fn new(references: Vec<MappingRange>) -> Self {
+        Self { references }
     }
 
     fn convert(&self, num: i64) -> i64 {
@@ -197,35 +184,25 @@ impl Map {
     }
 }
 
-impl From<&[String]> for Map {
+impl From<&[String]> for Mapping {
     fn from(value: &[String]) -> Self {
-        let mut names =
-            value.first()
-                .unwrap()
-                .split(' ')
-                .next()
-                .unwrap()
-                .split('-');
-
-        Map::new(
-            names.next().unwrap().to_string(),
-            names.next_back().unwrap().to_string(),
+        Mapping::new(
             value.iter()
                 .skip(1)
-                .map(|l| MapRange::from_str(l).unwrap())
+                .map(|l| MappingRange::from_str(l).unwrap())
                 .collect::<Vec<_>>(),
         )
     }
 }
 
 #[derive(Debug)]
-struct MapRange {
+struct MappingRange {
     destination_range_start: i64,
     source_range_start: i64,
     range_length: i64,
 }
 
-impl MapRange {
+impl MappingRange {
     pub fn new(destination_range_start: i64, source_range_start: i64, range_length: i64) -> Self {
         Self { destination_range_start, source_range_start, range_length }
     }
@@ -242,7 +219,7 @@ impl MapRange {
     }
 }
 
-impl FromStr for MapRange {
+impl FromStr for MappingRange {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
@@ -251,6 +228,6 @@ impl FromStr for MapRange {
                 .map(|s| s.parse::<i64>().unwrap())
                 .collect::<Vec<_>>();
 
-        Ok(MapRange::new(vec[0], vec[1], vec[2]))
+        Ok(MappingRange::new(vec[0], vec[1], vec[2]))
     }
 }
