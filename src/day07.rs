55a0fbbafbc4df47870e79b5174f6581a4289dc1
Marvin <marvk3@gmail.com>
2023-12-07 13:14:58 +0100

    7 refactoring

--- src/day07.rs
+++ src/day07.rs
@@ -20,7 +20,7 @@ impl Part<u64> for Part1 {
         6440
     }
 
-    fn solve(&self, input: &Vec<String>) -> u64 {
+    fn solve(&self, input: &[String]) -> u64 {
         let mut vec = input.into_iter()
             .filter(|s| !s.is_empty())
             .map(|l| {
@@ -48,7 +48,7 @@ impl Part<u64> for Part2 {
         5905
     }
 
-    fn solve(&self, input: &Vec<String>) -> u64 {
+    fn solve(&self, input: &[String]) -> u64 {
         let mut vec = input.into_iter()
             .filter(|s| !s.is_empty())
             .map(|l| {
@@ -66,7 +66,6 @@ impl Part<u64> for Part2 {
             .enumerate()
             .map(|(i, &Hand { bid, .. })| (i + 1) as u64 * bid as u64)
             .sum()
-
     }
 }
 
@@ -81,6 +80,64 @@ enum HandType {
     HighCard = 0,
 }
 
+impl HandType {
+    fn calculate_part_1(cards: &str) -> Self {
+        let distinct_chars = cards.chars().collect::<HashSet<_>>();
+
+        match distinct_chars.len() {
+            1 => Self::FiveOfAKind,
+            2 => {
+                let query_char = distinct_chars.iter().next().unwrap();
+                let query_count = cards.chars().filter(|c| c == query_char).count();
+
+                if query_count == 1 || query_count == 4 {
+                    Self::FourOfAKind
+                } else {
+                    Self::FullHouse
+                }
+            }
+            3 => {
+                let any_threes = distinct_chars.iter().map(|c1| cards.chars().filter(|c2| c1 == c2).count()).any(|v| v == 3);
+
+                if any_threes {
+                    Self::ThreeOfAKind
+                } else {
+                    Self::TwoPair
+                }
+            }
+            4 => Self::OnePair,
+            5 => Self::HighCard,
+            _ => panic!(),
+        }
+    }
+
+    fn calculate_part_2(cards: &str) -> Self {
+        let distinct_chars = cards.chars().filter(|&c| c != 'J').collect::<HashSet<_>>();
+        let distinct_count = distinct_chars.len();
+        let joker_count = cards.chars().filter(|&c| c == 'J').count();
+
+
+        match (joker_count, distinct_count) {
+            (5, _) | (4, _) | (_, 1) => Self::FiveOfAKind,
+            (3 | 2, 2) => Self::FourOfAKind,
+            (2 | 1, 3) => Self::ThreeOfAKind,
+            (1, 2) => {
+                let query_char = distinct_chars.iter().next().unwrap();
+                let query_count = cards.chars().filter(|c| c == query_char).count();
+
+                if query_count == 1 || query_count == 3 {
+                    Self::FourOfAKind
+                } else {
+                    Self::FullHouse
+                }
+            }
+            (1, 4) => Self::OnePair,
+            (0, _) => Self::calculate_part_1(cards),
+            _ => panic!(),
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Hand {
     cards: String,
@@ -91,119 +148,40 @@ struct Hand {
 
 impl Hand {
     pub fn new_part_1(cards: String, bid: u32) -> Self {
-        let hand_type = Self::calculate_hand_type_part_1(&cards);
+        let hand_type = HandType::calculate_part_1(&cards);
 
-        let card_values = Self::calculate_card_values_part_1(&cards);
+        let card_values = cards.chars().map(Self::calculate_card_value_part_1).collect();
         Self { cards, card_values, bid, hand_type }
     }
 
     pub fn new_part_2(cards: String, bid: u32) -> Self {
-        let hand_type = Self::calculate_hand_type_part_2(&cards);
+        let hand_type = HandType::calculate_part_2(&cards);
 
-        let card_values = Self::calculate_card_values_part_2(&cards);
+        let card_values = cards.chars().map(Self::calculate_card_value_part_2).collect();
         Self { cards, card_values, bid, hand_type }
     }
 
-    fn calculate_card_values_part_1(cards: &str) -> Vec<u32> {
-        cards.chars().map(|c| {
-            match c {
-                'A' => 14,
-                'K' => 13,
-                'Q' => 12,
-                'J' => 11,
-                'T' => 10,
-                _ if c.is_numeric() => c.to_digit(10).unwrap(),
-                _ => panic!(),
-            }
-        }).collect()
-    }
-
-    fn calculate_hand_type_part_1(cards: &str) -> HandType {
-        let distinct_chars = cards.chars().collect::<HashSet<_>>();
-
-        match distinct_chars.len() {
-            1 => HandType::FiveOfAKind,
-            2 => {
-                let query_char = distinct_chars.iter().next().unwrap();
-                let query_count = cards.chars().filter(|c| c == query_char).count();
-
-                if query_count == 1 || query_count == 4 {
-                    HandType::FourOfAKind
-                } else {
-                    HandType::FullHouse
-                }
-            }
-            3 => {
-                let any_threes = distinct_chars.iter().map(|c1| cards.chars().filter(|c2| c1 == c2).count()).any(|v| v == 3);
-
-                if any_threes {
-                    HandType::ThreeOfAKind
-                } else {
-                    HandType::TwoPair
-                }
-            }
-            4 => HandType::OnePair,
-            5 => HandType::HighCard,
+    fn calculate_card_value_part_1(c: char) -> u32 {
+        match c {
+            'A' => 14,
+            'K' => 13,
+            'Q' => 12,
+            'J' => 11,
+            'T' => 10,
+            _ if c.is_numeric() => c.to_digit(10).unwrap(),
             _ => panic!(),
         }
     }
 
-    fn calculate_card_values_part_2(cards: &str) -> Vec<u32> {
-        cards.chars().map(|c| {
-            match c {
-                'A' => 14,
-                'K' => 13,
-                'Q' => 12,
-                'T' => 10,
-                _ if c.is_numeric() => c.to_digit(10).unwrap(),
-                'J' => 0,
-                _ => panic!(),
-            }
-        }).collect()
-    }
-
-    fn calculate_hand_type_part_2(cards: &str) -> HandType {
-        let distinct_chars = cards.chars().filter(|&c| c != 'J').collect::<HashSet<_>>();
-        let distinct_count = distinct_chars.len();
-        let joker_count = cards.chars().filter(|&c| c == 'J').count();
-
-        match joker_count {
-            5 | 4 => HandType::FiveOfAKind,
-            3 => {
-                match distinct_chars.len() {
-                    1 => HandType::FiveOfAKind,
-                    2 => HandType::FourOfAKind,
-                    _ => panic!(),
-                }
-            }
-            2 => {
-                match distinct_chars.len() {
-                    1 => HandType::FiveOfAKind,
-                    2 => HandType::FourOfAKind,
-                    3 => HandType::ThreeOfAKind,
-                    _ => panic!(),
-                }
-            }
-            1 => {
-                match distinct_chars.len() {
-                    1 => HandType::FiveOfAKind,
-                    2 => {
-                        let query_char = distinct_chars.iter().next().unwrap();
-                        let query_count = cards.chars().filter(|c| c == query_char).count();
-
-                        if query_count == 1 || query_count == 3 {
-                            HandType::FourOfAKind
-                        } else {
-                            HandType::FullHouse
-                        }
-                    },
-                    3 => HandType::ThreeOfAKind,
-                    4 => HandType::OnePair,
-                    _ => panic!(),
-                }
-            }
-            0 => Self::calculate_hand_type_part_1(cards),
-            _ => panic!()
+    fn calculate_card_value_part_2(c: char) -> u32 {
+        match c {
+            'A' => 14,
+            'K' => 13,
+            'Q' => 12,
+            'T' => 10,
+            _ if c.is_numeric() => c.to_digit(10).unwrap(),
+            'J' => 0,
+            _ => panic!(),
         }
     }
 }
