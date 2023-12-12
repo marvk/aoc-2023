0b48dcbaf665b6147f7bfc503900c5f45ea2d2eb
Marvin <marvk3@gmail.com>
2023-12-12 10:50:22 +0100

    12 part2

--- src/day12.rs
+++ src/day12.rs
@@ -1,79 +1,90 @@
-use std::fmt::Display;
+use std::collections::HashMap;
 use std::str::FromStr;
-
-use regex::Regex;
 
 use crate::harness::{Day, Part};
 
-pub fn day12() -> Day<usize, usize> {
+pub fn day12() -> Day<u64, u64> {
     Day::new(12, Box::new(Part1 {}), Box::new(Part2 {}))
 }
 
 pub struct Part1;
 
-impl Part<usize> for Part1 {
-    fn expect_test(&self) -> usize {
+impl Part<u64> for Part1 {
+    fn expect_test(&self) -> u64 {
         21
     }
 
-    fn solve(&self, input: &[String]) -> usize {
-        let records = parse(input);
-
-        let mut total = 0;
-
-        for x in &records {
-            // dbg!(x);
-            // dbg!(x.unknown_count());
-            // dbg!(x.unknown_working_count());
-
-            let permutations = permutations(x.unknown_working_count(), x.unknown_count());
-
-
-            let mut count = 0;
-
-
-            for permutation in permutations {
-                let mut current = x.chars.clone();
-                permutation.iter()
-                    .map(|&i| x.unknown_positions[i])
-                    .for_each(|i| current[i] = '#');
-
-                // println!("{:?}", current);
-                let check1 = check(current.iter().collect::<String>().as_str(), &x.working_groups);
-
-                if check1 {
-                    count += 1;
-                }
-            }
-
-            total += count;
-        }
-
-        total
+    fn solve(&self, input: &[String]) -> u64 {
+        parse(input).into_iter()
+            .map(solve)
+            .sum()
     }
-}
-
-fn check(cogs: &str, working_groups: &[usize]) -> bool {
-    let lens = Regex::new(r"[^#]")
-        .unwrap()
-        .split(cogs)
-        .filter(|e| !e.is_empty())
-        .map(|e| e.len())
-        .collect::<Vec<_>>();
-
-    lens == working_groups
 }
 
 pub struct Part2;
 
-impl Part<usize> for Part2 {
-    fn expect_test(&self) -> usize {
-        todo!()
+impl Part<u64> for Part2 {
+    fn expect_test(&self) -> u64 {
+        525152
     }
 
-    fn solve(&self, input: &[String]) -> usize {
-        todo!()
+    fn solve(&self, input: &[String]) -> u64 {
+        parse(input).into_iter()
+            .map(|r| r.extend(5))
+            .map(solve)
+            .sum()
     }
+}
+
+fn solve(r: Record) -> u64 {
+    solve_rec(r.chars.as_slice(), r.working_groups.as_slice(), &mut HashMap::new(), 0, 0, 0)
+}
+
+fn solve_rec(chars: &[char], groups: &[usize], cache: &mut HashMap<(usize, usize, usize), u64>, char_index: usize, group_index: usize, current_group_size: usize) -> u64 {
+    let key = (char_index, group_index, current_group_size);
+
+    if let Some(result) = cache.get(&key) {
+        return *result;
+    }
+
+    let end_of_input = char_index == chars.len();
+
+    if end_of_input {
+        let all_groups_cleared = group_index == groups.len();
+        let not_currently_on_group = current_group_size == 0;
+
+        let on_last_group_and_current_size_is_last_group_size = group_index == (groups.len() - 1) && groups[group_index] == current_group_size;
+
+        return if (all_groups_cleared && not_currently_on_group) || (on_last_group_and_current_size_is_last_group_size) {
+            1
+        } else {
+            0
+        };
+    }
+
+    let current = chars[char_index];
+
+    let mut total = 0;
+
+    if current == '.' || current == '?' {
+        let not_currently_on_group = current_group_size == 0;
+
+        let current_group_is_not_out_of_bounds_and_current_group_is_completed = group_index < groups.len() && groups[group_index] == current_group_size;
+
+        if not_currently_on_group {
+            total += solve_rec(chars, groups, cache, char_index + 1, group_index, 0);
+        } else if current_group_is_not_out_of_bounds_and_current_group_is_completed {
+            total += solve_rec(chars, groups, cache, char_index + 1, group_index + 1, 0);
+        }
+    }
+
+    if current == '#' || current == '?' {
+        total += solve_rec(chars, groups, cache, char_index + 1, group_index, current_group_size + 1);
+    }
+
+    cache.insert(key, total);
+
+    total
 }
 
 fn parse(input: &[String]) -> Vec<Record> {
@@ -83,50 +94,25 @@ fn parse(input: &[String]) -> Vec<Record> {
         .collect()
 }
 
-fn permutations(selected: usize, n: usize) -> Vec<Vec<usize>> {
-    permutations_rec(vec![], 0, selected, n)
-}
-
-fn permutations_rec(previous: Vec<usize>, index: usize, remaining_selected: usize, total_length: usize) -> Vec<Vec<usize>> {
-    if remaining_selected == 0 {
-        return vec![previous];
-    } else if index == total_length {
-        return vec![];
-    }
-
-    let mut results = vec![];
-
-    results.extend_from_slice(&permutations_rec(previous.clone(), index + 1, remaining_selected, total_length));
-
-
-    let mut with_appended = previous.clone();
-    with_appended.push(index);
-    results.extend_from_slice(&permutations_rec(with_appended, index + 1, remaining_selected - 1, total_length));
-
-    results
-}
-
 #[derive(Debug)]
 struct Record {
-    raw: String,
     chars: Vec<char>,
     working_groups: Vec<usize>,
-    unknown_positions: Vec<usize>,
-    working_count: usize,
-    known_working_count: usize,
 }
 
 impl Record {
-    pub fn new(raw: String, chars: Vec<char>, working_groups: Vec<usize>, unknown_positions: Vec<usize>, working_count: usize, known_working_count: usize) -> Self {
-        Self { raw, chars, working_groups, unknown_positions, working_count, known_working_count }
+    fn new(chars: Vec<char>, working_groups: Vec<usize>) -> Self {
+        Self { chars, working_groups }
     }
 
-    fn unknown_working_count(&self) -> usize {
-        self.working_count - self.known_working_count
-    }
-
-    fn unknown_count(&self) -> usize {
-        self.unknown_positions.len()
+    fn extend(&self, factor: usize) -> Record {
+        Record::from_str(
+            &format!(
+                "{} {}",
+                vec![self.chars.iter().collect::<String>(); factor].join("?"),
+                vec![self.working_groups.clone(); factor].iter().flatten().map(|e| e.to_string()).collect::<Vec<_>>().join(",")
+            )
+        ).unwrap()
     }
 }
 
@@ -135,21 +121,19 @@ impl FromStr for Record {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut split = s.split(' ');
-        let chars = split.next().unwrap().chars().collect::<Vec<_>>();
-        let unknown_positions = chars.iter().enumerate().filter(|(_, c)| **c == '?').map(|(i, _)| i).collect::<Vec<_>>();
-        let working_groups = split.next().unwrap().split(',').map(|e| e.parse::<usize>().unwrap()).collect::<Vec<_>>();
 
-        let working_count = working_groups.iter().sum::<usize>();
-        let known_working_count = chars.iter().filter(|c| **c == '#').count();
+        let chars =
+            split.next().unwrap().chars()
+                .collect::<Vec<_>>();
 
+        let working_groups =
+            split.next().unwrap().split(',')
+                .map(|e| e.parse::<usize>().unwrap())
+                .collect::<Vec<_>>();
 
         Ok(Record::new(
-            s.to_string(),
             chars,
             working_groups,
-            unknown_positions,
-            working_count,
-            known_working_count,
         ))
     }
 }
