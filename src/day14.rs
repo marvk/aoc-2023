01692f28c149f3066eabed3b21f6bdc098869450
Marvin Kuhnke <marvin.kuhnke@iits-consulting.de>
2023-12-14 14:19:40 +0100

    14 refactoring

--- src/day14.rs
+++ src/day14.rs
@@ -1,6 +1,5 @@
-use std::collections::{HashMap, HashSet};
-use std::fmt::Display;
-use std::hash::{Hash, Hasher};
+use std::collections::HashMap;
+use std::hash::Hash;
 use std::ops::{Add, Sub};
 
 use crate::harness::{Day, Part};
@@ -37,27 +36,18 @@ impl Part<u64> for Part2 {
     }
 }
 
-#[derive(Eq, PartialEq, Debug)]
-struct Rocks(HashSet<Vec2>);
-
-impl Hash for Rocks {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        for x in &self.0 {
-            x.hash(state);
-        }
-    }
-}
-
 struct Map {
-    obstacles: HashSet<Vec2>,
-    rocks: HashSet<Vec2>,
+    raw: Vec<Vec<char>>,
     width: usize,
     height: usize,
 }
 
 impl Map {
-    fn new(obstacles: HashSet<Vec2>, rocks: HashSet<Vec2>, width: usize, height: usize) -> Self {
-        Self { obstacles, rocks, width, height }
+    fn new(raw: Vec<Vec<char>>) -> Self {
+        let width = raw[0].len();
+        let height = raw.len();
+
+        Self { raw, width, height }
     }
 
     fn make_cycle(&mut self) {
@@ -67,48 +57,76 @@ impl Map {
         self.make_step(Vec2::EAST);
     }
 
-    fn make_step(&mut self, direction: Vec2) {
-        loop {
-            let mut changes = false;
-
-            self.rocks = self.rocks.iter()
-                .map(|r| {
-                    let next = *r + direction;
-
-                    if next.y >= 0 && next.y < self.height as i64 && next.x >= 0 && next.x < self.width as i64 && !self.rocks.contains(&next) && !self.obstacles.contains(&next) {
-                        changes = true;
-                        next
-                    } else {
-                        *r
+    fn make_step(&mut self, delta: Vec2) {
+        match delta {
+            Vec2::NORTH | Vec2::WEST => {
+                for y in 0..self.height {
+                    for x in 0..self.width {
+                        self.slide_rock(x, y, &delta);
                     }
-                })
-                .collect();
-
-            if !changes {
-                break;
+                }
             }
+            Vec2::SOUTH | Vec2::EAST => {
+                for y in (0..self.height).rev() {
+                    for x in (0..self.width).rev() {
+                        self.slide_rock(x, y, &delta);
+                    }
+                }
+            }
+            _ => panic!()
         }
     }
 
+    fn slide_rock(&mut self, x: usize, y: usize, step: &Vec2) {
+        if self.raw[y][x] == ROCK {
+            let mut next_x = x as i64;
+            let mut next_y = y as i64;
+
+            loop {
+                next_x += step.x;
+                next_y += step.y;
+
+                if !self.is_in_bounds(next_x, next_y) || self.raw[next_y as usize][next_x as usize] != EMPTY {
+                    break;
+                }
+            }
+
+            self.raw[y][x] = EMPTY;
+            self.raw[(next_y - step.y) as usize][(next_x - step.x) as usize] = ROCK;
+        }
+    }
+
+    fn is_in_bounds(&self, next_x: i64, next_y: i64) -> bool {
+        next_y >= 0 && next_y < self.height as i64 && next_x >= 0 && next_x < self.width as i64
+    }
+
     fn make_cycles(&mut self, times: usize) {
-        let mut cache = HashMap::<Rocks, usize>::new();
-        cache.insert(Rocks(self.rocks.clone()), 0);
+        let mut cache = HashMap::<Vec<Vec<char>>, usize>::new();
+        cache.insert(self.raw.clone(), 0);
 
         let mut i = 0;
+
+        let mut cycle_found = false;
 
         while i < times {
             self.make_cycle();
 
-            let new_rocks = Rocks(self.rocks.clone());
+            let new_rocks = self.raw.clone();
 
-            if let Some(&last_occurrence) = cache.get(&new_rocks) {
-                let cycle_length = i - last_occurrence;
+            let option = cache.get(&new_rocks);
 
-                while i < times {
-                    i += cycle_length;
+            if let Some(&cycle_start) = option {
+                if !cycle_found {
+                    let cycle_length = i - cycle_start;
+
+                    while i <= times {
+                        i += cycle_length;
+                    }
+
+                    i -= cycle_length;
+
+                    cycle_found = true;
                 }
-
-                i -= cycle_length;
             } else {
                 cache.insert(new_rocks, i);
             }
@@ -118,43 +136,32 @@ impl Map {
     }
 
     fn count_load(&self) -> u64 {
-        self.rocks.iter()
-            .map(|r| r.y)
-            .map(|y| self.height as i64 - y)
-            .map(|x| x as u64)
+        self.raw.iter()
+            .enumerate()
+            .map(|(y, line)| line.iter().filter(|&&e| e == ROCK).count() as i64 * (self.height as i64 - y as i64))
+            .map(|e| e as u64)
             .sum()
     }
 }
 
+const ROCK: char = 'O';
+const EMPTY: char = '.';
+
 impl From<&[String]> for Map {
     fn from(value: &[String]) -> Self {
-        let value = value.iter().filter(|l| !l.is_empty()).collect::<Vec<_>>();
+        let raw =
+            value.iter()
+                .filter(|l| !l.is_empty())
+                .map(|line| line.chars().collect())
+                .collect();
 
-        let mut obstacles = HashSet::new();
-        let mut rocks = HashSet::new();
-
-        for (y, line) in value.iter().enumerate() {
-            for (x, char) in line.chars().enumerate() {
-                match char {
-                    'O' => rocks.insert(v_usize(x, y)),
-                    '#' => obstacles.insert(v_usize(x, y)),
-                    '.' => false,
-                    _ => panic!()
-                };
-            }
-        }
-
-        Map::new(obstacles, rocks, value[0].len(), value.len())
+        Map::new(raw)
     }
 }
 
 
 const fn v(x: i64, y: i64) -> Vec2 {
     Vec2::new(x, y)
-}
-
-const fn v_usize(x: usize, y: usize) -> Vec2 {
-    v(x as i64, y as i64)
 }
 
 #[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
@@ -171,10 +178,6 @@ impl Vec2 {
 
     pub const fn new(x: i64, y: i64) -> Self {
         Self { x, y }
-    }
-
-    pub const fn manhattan_dist(&self, other: &Vec2) -> i64 {
-        (self.x - other.x).abs() + (self.y - other.y).abs()
     }
 }
 
